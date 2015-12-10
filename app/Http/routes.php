8d85855069dd62a1980988d2a521877fb0ca6668
Ryan Kucinski <ryankucinski@gmail.com>
2015-12-09 22:00:29 -0500

    added recipe routes and user login routes with comments for future me

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,60 @@
 |
 */
 
+/*----------------------------------------------------
+/welcome page
+-----------------------------------------------------*/
+
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Route::get('/welcome', 'WelcomeController@getIndex');
+
+/*----------------------------------------------------
+/User Registration and Login / Logout
+-----------------------------------------------------*/
+
+# Show login form
+Route::get('/login', 'Auth\AuthController@getLogin');
+
+# Process login form
+Route::post('/login', 'Auth\AuthController@postLogin');
+
+# Process logout
+Route::get('/logout', 'Auth\AuthController@getLogout');
+
+# Show registration form
+Route::get('/register', 'Auth\AuthController@getRegister');
+
+# Process registration form
+Route::post('/register', 'Auth\AuthController@postRegister');
+
+
+/*----------------------------------------------------
+/recipes
+-----------------------------------------------------*/
+
+# Show recipe creation form
+Route::get('/recipes/create', 'RecipeController@getCreate');
+
+# Process  recipe creation form
+Route::post('/recipes/create', 'RecipeController@postCreate');
+
+# Show recipe Edit form
+Route::get('/recipes/edit/{id?}', 'RecipeController@getEdit');
+
+# Process recipe edit form
+Route::post('/recipes/edit', 'RecipeController@postEdit');
+
+# Show recipe confirm Delete form
+Route::get('/recipes/confirm-delete/{id?}', 'RecipeController@getConfirmDelete');
+
+# show recipe Delete form
+Route::get('/recipes/delete/{id?}', 'RecipeController@getDoDelete');
+
+#show recipes
+Route::get('/recipes', 'RecipeController@getIndex');
+
+#show a specific recipe
+Route::get('/recipes/show/{title?}', 'RecipeController@getShow');
