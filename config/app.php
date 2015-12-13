d6c801bf7c53a5f041e32e344290ced442fe4159
Ryan Kucinski <ryankucinski@gmail.com>
2015-12-13 17:30:19 -0500

    downloaded and setup debugbar package, configured local database and tested connection

--- config/app.php
+++ config/app.php
@@ -144,6 +144,7 @@ return [
         P4\Providers\AuthServiceProvider::class,
         P4\Providers\EventServiceProvider::class,
         P4\Providers\RouteServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -193,6 +194,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
