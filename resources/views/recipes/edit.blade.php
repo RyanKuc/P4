0059ab0b08518d0b6981ffd2d437581a8212d915
Ryan Kucinski <ryankucinski@gmail.com>
2015-12-16 18:29:53 -0500

    got edit template working fairly easily using most of the same form from create view and adding code for tag checkboxes

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -1,20 +1,50 @@
 @extends('layouts.master')
 
 @section('content')
-  <h1>Edit a Recipe</h1>
 
-  {{ $recipe->title }}
-  {{ $recipe->id }}
-  <div>
-    All Tags:
-    @foreach($tags_for_form as $tag_id => $tag)
-    <br>{{ $tag['tag_name'] }}
-    @endforeach
-    <br>
-    <br>
-    This recipes tags
-    @foreach($tags_for_this_recipe as $tag_name)
-    <br>{{ $tag_name }}
-    @endforeach
-  </div>
+<div class='col-sm-4'>
+  <form method='POST' action='/recipes/create' role='form'>
+
+    <input type='hidden' value='{{ csrf_token() }}' name='_token'>
+
+    <input type='hidden' value='{{ $recipe->id }}' name='id'>
+
+    <div class='form-group'>
+      <label for='title'>Title:</label>
+      <input type='text' class='form-control' id='title' name='title' value='{{ $recipe->title }}'>
+    </div>
+
+    <div class='form-group'>
+      <label for='picture_link'>Picture Link:</label>
+      <input type='url' class='form-control' id='picture_link' name='picture_link' value='{{ $recipe->picture_link }}'>
+    </div>
+
+    <div class='form-group'>
+      <label for='description'>Description:</label>
+      <input type='text' class='form-control' id='description' name='description' maxlength='200' value='{{ $recipe->description }}'>
+    </div>
+
+    <div class='form-group'>
+      <label for='ingredients'>Ingredients:</label>
+      <input type='text' class='form-control' id='ingredients' name='ingredients' value='{{ $recipe->ingredients }}'>
+    </div>
+
+    <div class='form-group'>
+      <label for='instructions'>Instructions:</label>
+      <input type='text' class='form-control' id='instructions' name='instructions' value='{{ $recipe->instructions }}'>
+    </div>
+
+    <div class='form-group'>
+      <label for='tags'>Tags:</label>
+      @foreach($tags_for_form as $tag_id => $tag)
+      <?php $checked = (in_array($tag['tag_name'],$tags_for_this_recipe)) ? 'CHECKED' : ''?>
+      <br><input {{ $checked }} type='checkbox' name='tags[]' value='{{$tag_id}}'> {{ $tag['tag_name'] }}
+      @endforeach
+    </div>
+
+    <button type='submit' class='btn btn-success'>Submit</button>
+
+  </form>
+</div>
+
 @stop
